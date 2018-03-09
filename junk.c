6ca9191c32b94b89da301af2593114eac785bf9b
iluvhalo <hgd145@vols.utk.edu>
2018-03-08 22:01:57 -0500

    I've got 1-3 working, now working on 4

--- junk.c
+++ junk.c
@@ -1,98 +1,120 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string.h> // REMOVE THIS
 #include "mymalloc.h"
 
 typedef struct flist {
-   int size;
-   struct flist *flink;
+  int size;
+  struct flist *flink;
+  struct flist *blink;
 } *Flist;
 
 Flist FL;
 
 void *my_malloc(size_t size) {
-   Flist tmp, set;
-   void *mallocBegin;
-   void *ret;
-   int pad, r, t;
+  Flist setb, set, rem;
+  void *beg, *ret;
+  int pad, t;
 
-   if (FL == NULL) {
-      FL = (Flist) sbrk(8192);
-      FL->size = 8192;
-      FL->flink = NULL;
-   }
+  if (FL == NULL) {
+    FL = (Flist) sbrk(8192);
+    FL->size = 8192;
+    FL->flink = NULL;
+    FL->blink = NULL;
+  }
 
+  if ((size % 8) == 0) {
+    pad = size + 8;
+  } else {
+    pad = size + 8 + (8 - (size % 8));
+  }
 
-   r = size % 8;
-   if (r == 0) {
-      pad = size + 8;
-   } else {
-      pad = size + 8 + (8 - r);
-   }
+  // if freeList is more than one element
+  if (FL->flink != NULL) {
+    set = NULL;
+    for (set = free_list_begin(); set->size < pad; set = free_list_next(set)) {}
+    if (set->size < pad) printf("more than one element, but none of them big enough\n");
 
-   mallocBegin = NULL;
-   for (tmp = free_list_begin(); tmp != NULL; tmp = free_list_next(tmp)) {
-      if (tmp->size >= pad) {
-         mallocBegin = (void *) tmp;
-         break;
-      }
-   }
+    beg = (void *) set;
+    ret = beg;
+    beg += pad;
+    rem = (Flist) beg;
+    //setb->flink = rem;
+  } else if (FL->size > pad) {
+    // FL is one element and is big enough
+    set = FL;
+    beg = (void *) set;
+    ret = beg;
+    beg += pad;
+    rem = (Flist) beg;
 
-   if (mallocBegin == NULL) {
-      printf("Need to call sbrk() and append it to the end of the currently allocated memory\n");
-   }
+    //    printf("\nsetb: 0x%x\nset: 0x%x\nret: 0x%x\nbeg: 0x%x\nrem: 0x%x\n", setb, set, ret, beg, rem);
 
-   set = tmp;
-   ret = mallocBegin;
-   mallocBegin += pad;
+    //    setb = NULL;
+  } else if (FL->size == pad) {
+    printf("just big enough\n"); 
+  } else {
+    // FL is not big enough
+    printf("need to call sbrk() to make bigger heap\n");
+  }
+  //  printf("After if/else if\n");
 
-   t = set->size;
-   set->size = pad;
-   set->flink = NULL;
-   if((mallocBegin - pad) == FL) FL = mallocBegin;
-   set = mallocBegin;
-   tmp = mallocBegin;
-   set->size = t - pad;
-   set->flink = NULL;
-   ret += 8;
+  setb = NULL;
+  t = set->size;
+  set->size = pad;
+  if (set->blink != NULL) {
+    setb = set->blink;
+    set->blink->flink = set->flink;
+  }
+  set->flink = NULL;
+  set->blink = NULL;
+  if((beg - pad) == FL) {
+    FL = beg;
+  }
+  rem->size = t - pad;
+  rem->flink = NULL;
+  rem->blink = setb;
+  if (setb != NULL) setb->flink = rem;
+  //if (setb != NULL) {
+  //setb->flink = rem;
+  // }
+  ret += 8;
 
-   return ret;
-}
+  return ret;
+} // end of my_malloc()
 
 void my_free(void *ptr) {
-   Flist f, tmp;
+  Flist f, tmp;
 
-   ptr -= 8;
+  ptr -= 8;
 
-   f = ptr;
+  f = ptr;
 
-   printf("f->size: %d\n", f->size);
-   
-   for (tmp = (Flist) free_list_begin(); tmp != NULL; tmp = (Flist) free_list_next(tmp)) {
-      if (tmp->flink == NULL) {
-         printf("Reached end of list\n");
-         f->flink = tmp;
-         //f->size += tmp->size;
-         //tmp->size = 0;
-         FL = f;
-         return;
-      }
-      printf("block address: 0x%x\n", tmp);
-      printf("   block size: %d\n", tmp->size);
-      printf("   block flink: 0x%x\n", tmp->flink);
-   }
+  //  printf("f->size: %d\n", f->size);
 
+  for (tmp = (Flist) free_list_begin(); tmp != NULL; tmp = (Flist) free_list_next(tmp)) {
+    if (tmp > f) {
+      // found the next node after ptr in the FL
+      f->flink = tmp;
+      if (tmp->blink != NULL) tmp->blink->flink = f;
+      f->blink = tmp->blink;
+      tmp->blink = f;
 
-}
+      if (FL == tmp) FL = f;
+      
+      return;
+    }
+  }
+} // end of my_free()
 
 void *free_list_begin() {
-   return (void *) FL;
+  return (void *) FL;
 }
 
 void *free_list_next(void *node) {
-   Flist n;
-   n = (Flist) node;
-   return (void *) n->flink;
+  Flist n;
+  n = (Flist) node;
+  return (void *) n->flink;
 }
 
 void coalesce_free_list() {
@@ -101,77 +123,67 @@ void coalesce_free_list() {
 
 void double_check_memory(int **ptrs, int *dc, int nptrs, int fl_size)
 {
-   void *low, *l;
-   void *high, *h;
-   int *ip, i;
-   int nbytes;
-   int nfl;
-   Flist thing;
+  void *low, *l;
+  void *high, *h;
+  int *ip, i;
+  int nbytes;
+  int nfl;
 
-   nbytes = 0;
+  nbytes = 0;
 
-   for (i = 0; i < nptrs; i++) {
-      l = (void *) ptrs[i];
-      thing = (Flist) ptrs[i];
-      printf("size of alloc'ed  node: %d\n", thing->size);
-      l -= 8;
-      ip = (int *) l;
+  for (i = 0; i < nptrs; i++) {
+    l = (void *) ptrs[i];
+    l -= 8;
+    ip = (int *) l;
 
+    if (*ip != dc[i]) {
+      printf("Error: pointer number %d the wrong size (%d instead of %d)\n", i, *ip, dc[i]);
+      exit(1);
+    }
+    h = l + *ip;
+    if (nbytes == 0 || l < low) low = l;
+    if (nbytes == 0 || h > high) high = h;
+    nbytes += *ip;
+  }
 
+  nfl = 0;
+  for (l = free_list_begin(); l != NULL; l = free_list_next(l)) {
+    ip = (int *) l;
+    h = l + *ip;
+    if (nbytes == 0 || l < low) low = l;
+    if (nbytes == 0 || h > high) high = h;
+    nbytes += *ip;
+    nfl++;
+  }
 
-      if (*ip != dc[i]) {
-         printf("Error: pointer number %d the wrong size (%d instead of %d)\n", i, *ip, dc[i]);
-         exit(1);
-      }
-      h = l + *ip;
-      if (nbytes == 0 || l < low) low = l;
-      if (nbytes == 0 || h > high) high = h;
-      nbytes += *ip;
-   }
+  if (nbytes != 8192) {
+    printf("Error: Total bytes allocated and on the free list = %d, not 8192\n", nbytes);
+    exit(0);
+  }
 
-   nfl = 0;
-   for (l = free_list_begin(); l != NULL; l = free_list_next(l)) {
-      thing = (Flist) l;
-      printf("size of free list node: %d\n", thing->size);
-      ip = (int *) l;
-      h = l + *ip;
-      if (nbytes == 0 || l < low) low = l;
-      if (nbytes == 0 || h > high) high = h;
-      nbytes += *ip;
-      nfl++;
-   }
+  if (high - low != 8192) {
+    printf("Error: Highest address (0x%x) minus lowest (0x%x) does not equal 8192\n", (int) high, (int) low);
+    exit(0);
+  }
 
-   if (nbytes != 8192) {
-      printf("Error: Total bytes allocated and on the free list = %d, not 8192\n", nbytes);
-      exit(0);
-   }
-
-   if (high - low != 8192) {
-      printf("Error: Highest address (0x%x) minus lowest (0x%x) does not equal 8192\n", (int) high, (int) low);
-      exit(0);
-   }
-
-   if (nfl != fl_size && fl_size != -1) {
-      printf("Error: %d nodes on the free list -- should be %d\n", nfl, fl_size);
-      exit(0);
-   }
+  if (nfl != fl_size && fl_size != -1) {
+    printf("Error: %d nodes on the free list -- should be %d\n", nfl, fl_size);
+    exit(0);
+  }
 }
-
 
 
 main()
 {
-   int *ptrs[2];
-   int dc[2];
+  int *ptrs[2];
+  int dc[2];
 
-   ptrs[0] = my_malloc(23);
-   dc[0] = 32;
-   ptrs[1] = my_malloc(101);
-   my_free(ptrs[1]);
-   ptrs[1] = my_malloc(108);
-   dc[1] = 120;
+  ptrs[0] = my_malloc(23);
+  dc[0] = 32;
+  ptrs[1] = my_malloc(8145);
+  dc[1] = 8160;
 
-   double_check_memory(ptrs, dc, 2, 2);
-   printf("Correct\n");
+  double_check_memory(ptrs, dc, 2, 0);
+  printf("Correct\n");
 }
 
--- mymalloc.c
+++ mymalloc.c
@@ -1,56 +1,120 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h> // REMOVE THIS
 #include "mymalloc.h"
 
 typedef struct flist {
-   int size;
-   struct flist *flink;
+  int size;
+  struct flist *flink;
+  struct flist *blink;
 } *Flist;
 
-Flist freeList;
+Flist FL;
 
 void *my_malloc(size_t size) {
-   Flist mallocBegin;
-   char *mallocEnd;
-   int pad, r;
+  Flist setb, set, rem;
+  void *beg, *ret;
+  int pad, t;
 
-   if (freeList->flink == NULL) {
-      mallocBegin = (Flist) sbrk(8192);
-      freeList->size = 0;
-      freeList->flink = mallocBegin;
-      mallocBegin->size = 8192;
-      mallocBegin->flink = freeList;
-   }
+  if (FL == NULL) {
+    FL = (Flist) sbrk(8192);
+    FL->size = 8192;
+    FL->flink = NULL;
+    FL->blink = NULL;
+  }
 
+  if ((size % 8) == 0) {
+    pad = size + 8;
+  } else {
+    pad = size + 8 + (8 - (size % 8));
+  }
 
+  // if freeList is more than one element
+  if (FL->flink != NULL) {
+    set = NULL;
+    for (set = free_list_begin(); set->size < pad; set = free_list_next(set)) {}
+    if (set->size < pad) printf("more than one element, but none of them big enough\n");
 
-   r = size % 8;
-   if (r == 0) {
-      pad = size + 8;
-   } else {
-      pad = size + 8 + (8 - r);
-   }
-   printf("memory to allocate: %d\n", pad);
+    beg = (void *) set;
+    ret = beg;
+    beg += pad;
+    rem = (Flist) beg;
+    //setb->flink = rem;
+  } else if (FL->size > pad) {
+    // FL is one element and is big enough
+    set = FL;
+    beg = (void *) set;
+    ret = beg;
+    beg += pad;
+    rem = (Flist) beg;
 
-   printf("mallocBegin: 0x%x\n", mallocBegin);
-   printf("mallocEnd: 0x%x\n", mallocEnd);
+    //    printf("\nsetb: 0x%x\nset: 0x%x\nret: 0x%x\nbeg: 0x%x\nrem: 0x%x\n", setb, set, ret, beg, rem);
 
-   return (void *) freeList;
-}
+    //    setb = NULL;
+  } else if (FL->size == pad) {
+    printf("just big enough\n"); 
+  } else {
+    // FL is not big enough
+    printf("need to call sbrk() to make bigger heap\n");
+  }
+  //  printf("After if/else if\n");
+
+  setb = NULL;
+  t = set->size;
+  set->size = pad;
+  if (set->blink != NULL) {
+    setb = set->blink;
+    set->blink->flink = set->flink;
+  }
+  set->flink = NULL;
+  set->blink = NULL;
+  if((beg - pad) == FL) {
+    FL = beg;
+  }
+  rem->size = t - pad;
+  rem->flink = NULL;
+  rem->blink = setb;
+  if (setb != NULL) setb->flink = rem;
+  //if (setb != NULL) {
+  //setb->flink = rem;
+  // }
+  ret += 8;
+
+  return ret;
+} // end of my_malloc()
 
 void my_free(void *ptr) {
+  Flist f, tmp;
 
-}
+  ptr -= 8;
+
+  f = ptr;
+
+  //  printf("f->size: %d\n", f->size);
+
+  for (tmp = (Flist) free_list_begin(); tmp != NULL; tmp = (Flist) free_list_next(tmp)) {
+    if (tmp > f) {
+      // found the next node after ptr in the FL
+      f->flink = tmp;
+      if (tmp->blink != NULL) tmp->blink->flink = f;
+      f->blink = tmp->blink;
+      tmp->blink = f;
+
+      if (FL == tmp) FL = f;
+      
+      return;
+    }
+  }
+} // end of my_free()
 
 void *free_list_begin() {
-   return (void *) freeList->flink;
+  return (void *) FL;
 }
 
 void *free_list_next(void *node) {
-   Flist n;
-   n = (Flist) node;
-   n = n->flink;
-   return (void *) n;
+  Flist n;
+  n = (Flist) node;
+  return (void *) n->flink;
 }
 
 void coalesce_free_list() {
